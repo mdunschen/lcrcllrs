3076786a28bee6c6cd70a4777e351de2cf338e0c
mdunschen <mdunschen@gmail.com>
2018-01-23 12:26:36 +0000

    Update index.js

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,6 @@
 const lcrcllrsRoutes = require('./lcrcllrs_routes');
-console.log('index.js');
 
 module.exports = function(app, db) {
-	  console.log('calling lcrcllrsRoutes');
 	  lcrcllrsRoutes(app, db);
 	  // Other route groups could go here, in the future
 	};
